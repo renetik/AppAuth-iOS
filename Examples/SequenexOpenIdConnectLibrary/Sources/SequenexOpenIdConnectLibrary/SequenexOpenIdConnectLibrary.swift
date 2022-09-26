db03d9af5ca424dd2634ec4965e52b312aa135d8
Rene Dohan <dohan.rene@gmail.com>
2022-09-26 17:44:07 +0200

    changed interface

--- Examples/SequenexOpenIdConnectLibrary/Sources/SequenexOpenIdConnectLibrary/SequenexOpenIdConnectLibrary.swift
+++ Examples/SequenexOpenIdConnectLibrary/Sources/SequenexOpenIdConnectLibrary/SequenexOpenIdConnectLibrary.swift
@@ -13,7 +13,7 @@ protocol OIDCAuthorizer {
     // if token expired and we have refresh token, use it to get new access token
     // NOTE: refreshing token is not part of this story
     // NOTE: permanent storage of tokens is not part of this story
-    var token: String { get async throws }
+    func login() async throws -> String
 
     // resume login flow when app is opened from external browser
     // this might not be needed if we show embedded webview within the app
@@ -25,13 +25,41 @@ protocol OIDCAuthorizer {
     func logout()
 }
 
-struct OIDCConfiguration: Codable {
+struct OIDCConfiguration {
     var issuer: String
     var clientID: String?
     var redirectURI: URL
+    var barTintColor: UIColor?
+    var controlTintColor: UIColor?
 }
 
 enum OIDCError: LocalizedError {
     case loginCancelled
     case offline
+    case networkError(cause: Error)
 }
+
+//import UIKit
+//
+//extension Decodable where Self: UIColor {
+//
+//    public init(from decoder: Decoder) throws {
+//        let container = try decoder.singleValueContainer()
+//        let components = try container.decode([CGFloat].self)
+//        self = Self.init(red: components[0], green: components[1],
+//                         blue: components[2], alpha: components[3])
+//    }
+//}
+//
+//extension Encodable where Self: UIColor {
+//    public func encode(to encoder: Encoder) throws {
+//        var r, g, b, a: CGFloat
+//        (r, g, b, a) = (0, 0, 0, 0)
+//        var container = encoder.singleValueContainer()
+//        self.getRed(&r, green: &g, blue: &b, alpha: &a)
+//        try container.encode([r, g, b, a])
+//    }
+//
+//}
+//
+//extension UIColor: Codable { }
